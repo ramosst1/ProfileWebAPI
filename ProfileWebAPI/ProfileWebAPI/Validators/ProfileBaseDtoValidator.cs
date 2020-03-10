8a40f24b256e41961133c7bf5aa8fa8908216bf7
ramosst@aol.com <ramostt@aol.com>
2020-03-10 09:23:23 -0400

    More code changes.

--- ProfileWebAPI/ProfileWebAPI/Validators/ProfileBaseDtoValidator.cs
+++ ProfileWebAPI/ProfileWebAPI/Validators/ProfileBaseDtoValidator.cs
@@ -13,9 +13,9 @@ namespace ProfileWebAPI.Validators
         {
 
             RuleFor(field => field.FirstName).NotEmpty().WithMessage("{PropertyName} is required.")
-                .Length(2, 50).WithMessage("{PropertyName} Must be between {MinLength} and {MaxLength} characters ");
+                .Length(2, 50).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters ");
             RuleFor(field => field.LastName).NotEmpty().WithMessage("{PropertyName} is required.")
-                .Length(2, 50).WithMessage("{PropertyName} Must be between {MinLength} and {MaxLength} characters ");
+                .Length(2, 50).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength} characters ");
 
         }
     }
