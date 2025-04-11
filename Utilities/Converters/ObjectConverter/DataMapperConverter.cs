25c93ab135e1d60baad1f6752a340b1e06c9b0f4
ramosst1 <ramosst@aol.com>
2025-04-10 22:02:53 -0400

    More refactoring

--- Utilities/Converters/ObjectConverter/DataMapperConverter.cs
+++ Utilities/Converters/ObjectConverter/DataMapperConverter.cs
@@ -8,11 +8,11 @@
         {
             if (source == null) return default;
 
-            string jsonString = System.Text.Json.JsonSerializer.Serialize(source);
-
-            TTarget? response = System.Text.Json.JsonSerializer.Deserialize<TTarget>(jsonString);
-
-            return response;
+            return System.Text.Json.JsonSerializer.Deserialize<TTarget>(
+                System.Text.Json.JsonSerializer.Serialize(source)
+            );
         }
     }
 }
+
+
